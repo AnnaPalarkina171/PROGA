f62c83280506caf3c774574bbf887cbb55ebf8db
AnnaPalarkina171 <31884615+AnnaPalarkina171@users.noreply.github.com>
2019-06-14 02:06:45 +0300

    hw

--- HomeWorks/PROJECT/my_app.py
+++ HomeWorks/PROJECT/my_app.py
@@ -1,11 +1,22 @@
 from flask import Flask
 from flask import url_for, render_template, request, redirect
 import markovify
+import os
+import nltk
+import json
 
+#Чисто теоретически, можно сохранить модель в json и каждый раз использовать уже сгенерированную модель,
+#но на практике оказалось, что доставать фразы из уже сохраненной модели дольше, чем генерировать каждый раз.
 
-# Print three randomly-generated sentences of no more than 140 characters
-#for i in range(3):
-#    print(text_model.make_short_sentence(140))
+#corpus = open("GP.txt").read()
+#text_model = markovify.Text(text, state_size=3)
+#model_json = text_model.to_json()
+
+#with open('model.json','w', encoding='utf-8') as outfile:
+#    json.dump(model_json ,outfile)
+#with open('model.json') as file_json:
+#    model_json = json.load(file_json)
+#    reconstituted_model = markovify.Text.from_json(model_json)
 
 
 app = Flask(__name__)
@@ -31,3 +42,20 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=False)
+    app.debug = True
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
+
+
+#можно еще улучшить качество генерируемых предложений
+#но это ОЧЕНЬ долго
+
+#class POSifiedText(markovify.Text):
+#   def word_split(self, sentence):
+#        words = re.split(self.word_split_pattern, sentence)
+#        words = [ "::".join(tag) for tag in nltk.pos_tag(words) ]
+#        return words
+#   def word_join(self, words):
+#        sentence = " ".join(word.split("::")[0] for word in words)
+#        return sentence
